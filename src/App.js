9c67b52ff8d8701d802939669d15c83725ee7288
rustygwapoforever <rustyyandrielovpanal@gmail.com>
2024-02-18 08:58:53 +0800

    yeah

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
 
+// Step 4
 function App() {
+  // Step 1
+  const [expenses, setExpenses] = useState([]);
+
+  // Step 2
+  const fetchExpenses = async () => {
+    const apiUrl = "https://yawayawa.onrender.com";
+
+    const endpoint = `${apiUrl}/api/expenses`;
+
+    const response = await fetch(endpoint);
+
+    const expenseData = await response.json();
+
+    setExpenses(expenseData);
+  };
+
+  // Step 3
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
+
+  
+  function renderExpenses(expenses) {
+    const rows = expenses.map((expense) => (
+      <tr key={expense._id}>
+        <td>{expense._id}</td>
+        <td>{expense.description}</td>
+        <td>{expense.amount}</td>
+        <td>{new Date(expense.date).toDateString()}</td>
+      </tr>
+    ));
+
+    return rows;
+  }
+
   return (
     <div>
-    <form>
-      <textarea cols="30" rows="10"></textarea>
-      <input type="number" />
-      <input type="date" />
-      <button>Save</button>
-    </form>
-    
+      <form>
+        <textarea cols="30" rows="10"></textarea>
+        <input type="number" />
+        <input type="date" />
+        <button>Save</button>
+      </form>
+
       <h2>My Expenses</h2>
 
       <table width="100%">
@@ -24,9 +59,11 @@ function App() {
         </thead>
 
         <tbody>
+          {/* Rendering expenses */}
+          {renderExpenses(expenses)}
         </tbody>
       </table>
-  </div>
+    </div>
   );
 }
 
